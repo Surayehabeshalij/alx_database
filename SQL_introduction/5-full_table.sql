0f62c2f184da2e8bdae4d0861cbc7d39c8dac1df
Surayehabeshalij <surafelshemsu@gmail.com>
2023-08-14 15:48:19 +0300

    SQL

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,3 +1,3 @@
 -- Prints the full description of the table first_table from the database
 -- Query to print full description of a table
-SHOW CREATE TABLE first_table from hbtn_0c_0;
+SHOW CREATE TABLE first_table FROM hbtn_0c_0;
